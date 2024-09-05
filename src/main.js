f4ee9daa4b9473d5a8a7cc193e294a736e512277
neorise <neorise456@gmail.com>
2024-09-05 16:31:30 -0500

    chore: add dependencies configuration in main file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,47 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import Aura from '@primevue/themes/aura';
 
+import i18n from "./i18n.js";
+import SelectButton from "primevue/selectbutton";
+import Drawer from "primevue/drawer";
+import Avatar from "primevue/avatar";
+import Menu from "primevue/menu";
+import Toolbar from "primevue/toolbar";
+import Card from "primevue/card";
+import Button from "primevue/button";
+import Menubar from "primevue/menubar";
+
+
+//create app instance
+const app =createApp(App);
+
+//use PrimeVue
+app.use(PrimeVue, {
+    ripple: true, theme:{
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: 'false'
+        }
+    }
+});
+
+//Specify PrimeVue components to be used
+app.component('pv-card', Card)
+    .component('pv-button', Button)
+    .component('pv-select-button', SelectButton)
+    .component('pv-drawer', Drawer)
+    .component('pv-avatar', Avatar)
+    .component('pv-menu', Menu)
+    .component('pv-menubar', Menubar)
+    .component('pv-toolbar', Toolbar);
+
+
+//Mount app instance into #app div
 createApp(App).mount('#app')
